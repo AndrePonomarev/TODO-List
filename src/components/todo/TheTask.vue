d3d8e98b057a1e9b7ff2202a2df14576a576d878
Понаморев Андрей Анатольевич <cr_ovr_web3@efko.ru>
2024-01-11 11:12:45 +0300

    внесены ComplexityDots в задачи

--- src/components/todo/TheTask.vue
+++ src/components/todo/TheTask.vue
@@ -6,7 +6,14 @@
       <img src="../../../src/assets/img/edit-icon.png" alt="Edit">
     </button> </h3>
     <p class="task-item_description">{{ task.description }}</p>
-
+    <div class="task-item__complexity complexity">
+      <span
+        v-for="dot in getComplexityDots(task.plannedCompletionAt)"
+        :key="dot.id"
+        class="complexity__dot"
+        :class="dot.class"
+      ></span>
+    </div>
     <span class="task-item__datetime">{{ task.plannedCompletionAt }}</span>
     
     <button class="delete-task-button" @click="deleteTask">❌
@@ -18,9 +25,7 @@
 </template>
   
 <script>
-import axios from '../../utils/axios'
-import Modal from "./TheModal.vue";
-import TheColumn from "./TheColumn.vue";
+
 export default {
 
 
@@ -46,22 +51,8 @@ export default {
       this.$emit("delete-task", this.task.id);
     },
    
-
     openEditTaskModal(taskId) {
-      
-      
       this.$emit("open-edit-task-modal", this.task);
-
-      // console.log(taskId)
-      // const updatedTask = {
-      //   id: this.taskToUpdateId,
-      //   name: this.taskName,
-      //   description: this.taskDescription,
-      //   plannedCompletionAt: this.taskDate,
-      // };
-
-      
-
     },
 
     startDrag(event) {
@@ -72,11 +63,49 @@ export default {
     endDrag(event) {
       this.$emit("dragend", event); // Передача события в родительский компонент
     },
+
+    getComplexityDots(plannedCompletionAt) {
+      const currentDate = new Date();
+      const dueDate = new Date(plannedCompletionAt);
+      const timeDiff = dueDate.getTime() - currentDate.getTime();
+      const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      const dots = [];
+      if (daysDiff <= 7) {
+        dots.push({ id: 1, class: 'red-dot' });
+      } else if (daysDiff <= 14) {
+        dots.push({ id: 2, class: 'yellow-dot' });
+      } else {
+        dots.push({ id: 3, class: 'green-dot' });
+      }
+
+      return dots;
+    },
   },
 };
 </script>
   
 <style scoped>
-/* Ваши стили для компонента */
+
+.complexity__dot {
+  width: 24px;
+    height: 12px;
+    flex-shrink: 0;
+    background-color: #d9d9d9; 
+    border-radius: 0px 10px 10px 24px;
+    display: inline-block;
+    margin-right: 5px;
+}
+
+.green-dot {
+  background-color: #2ce49d; 
+}
+
+.yellow-dot {
+  background-color: #FDE047;
+}
+
+.red-dot {
+  background-color: #e42c5f; 
+}
 </style>
   
